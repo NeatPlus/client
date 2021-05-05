1e652bc157a402f6fae9a117f6ec0e0ca1d600a8
Dipesh Lamichhane <ft9dipesh@gmail.com>
2021-05-05 13:10:09 +0545

    Login and Register pages

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,8 @@
+import {Link} from 'react-router-dom';
+
 import styles from './styles.scss';
 
 import Button from 'components/Button';
-import Tabs, {Tab} from 'components/Tabs';
 
 const Home = () => {
     return (
@@ -12,15 +13,9 @@ const Home = () => {
             <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                 Learn React
             </a>
-            <Button className={styles.button}>Test Button</Button>
-            <Tabs className={styles.tabs}>
-                <Tab label="tab1" title="Tab 1">
-                    Testing Tab 1 Content
-                </Tab>
-                <Tab label="tab2" title="Tab 2">
-                    Testing Tab 2 Content
-                </Tab>
-            </Tabs>
+            <Link to="/login">
+                <Button className={styles.button}>Go to Login</Button>
+            </Link>
         </div>
     );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,16 @@ import {Route, Switch} from 'react-router-dom';
 import Toast from 'components/Toast';
 
 import Home from 'containers/Home';
+import Login from 'containers/Login';
+import Register from 'containers/Register';
 
 const Routes = () => {
     return (
         <>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
             </Switch>
             <Toast />
         </>
