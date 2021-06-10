894e80d73fbb66a50e26230bcccd7408b4831c93
saugatdhimal <dhimalsaugat77@gmail.com>
2021-06-10 13:15:16 +0545

    Added edit, clone and delete functionality

--- src/components/NoProject/index.js
+++ src/components/NoProject/index.js
@@ -4,53 +4,71 @@ import {useSelector} from 'react-redux';
 import {BsPlus} from 'react-icons/bs';
 
 import Button from 'components/Button';
-import CreateProjectModal from 'components/CreateProjectModal';
+import CreateEditProjectModal from 'components/CreateEditProjectModal';
 
 import noProjectImage from 'assets/images/no-project.svg';
 
 import styles from './styles.scss';
 
 const NoProject = () => {
-    const {user} = useSelector(state => state.auth);
+    const {user} = useSelector((state) => state.auth);
 
     const [showCreateModal, setShowCreateModal] = useState(false);
 
-    const handleShowCreateModal = useCallback(() => setShowCreateModal(true), []);
-    const handleHideCreateModal = useCallback(() => setShowCreateModal(false), []);
+    const handleShowCreateModal = useCallback(
+        () => setShowCreateModal(true),
+        []
+    );
+    const handleHideCreateModal = useCallback(
+        () => setShowCreateModal(false),
+        []
+    );
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Projects</h1>
             <main className={styles.content}>
                 <div className={styles.createBox}>
-                    <img src={noProjectImage} alt="No Projects" className={styles.infoImage} />
+                    <img
+                        src={noProjectImage}
+                        alt='No Projects'
+                        className={styles.infoImage}
+                    />
                     <p className={styles.infoTextTitle}>Hi {user.firstName},</p>
-                    <p className={styles.infoText}>Let's get started by creating a new project</p>
-                    <Button className={styles.button} onClick={handleShowCreateModal}>
-                        <BsPlus size={24} className={styles.buttonIcon} /> Create
+                    <p className={styles.infoText}>
+                        Let's get started by creating a new project
+                    </p>
+                    <Button
+                        className={styles.button}
+                        onClick={handleShowCreateModal}
+                    >
+                        <BsPlus size={24} className={styles.buttonIcon} />{' '}
+                        Create
                     </Button>
                 </div>
             </main>
-            <CreateProjectModal isVisible={showCreateModal} onClose={handleHideCreateModal} />
+            <CreateEditProjectModal
+                isVisible={showCreateModal}
+                onClose={handleHideCreateModal}
+                mode='create'
+            />
         </div>
     );
 };
 
 export default NoProject;
 
-export const withNoProject = WrappedComponent => {
+export const withNoProject = (WrappedComponent) => {
     const WithNoProject = (props) => {
-        const {projects, status} = useSelector(state => state.project);
-        if(projects.length) {
+        const {projects, status} = useSelector((state) => state.project);
+        if (projects.length) {
             return <WrappedComponent {...props} />;
         }
-        if(status!=='complete') {
+        if (status !== 'complete') {
             return null;
         }
 
         return <NoProject />;
-
     };
     return hoistNonReactStatics(WithNoProject, WrappedComponent);
 };
-
