7622cb76183adec3f2f78bb067f878aab2b093f2
Bishal Timilsina <bishaltimilsina@gmail.com>
2021-07-01 16:17:50 +0545

    Google Analytics

--- src/services/bootstrap.js
+++ src/services/bootstrap.js
@@ -1,9 +1,13 @@
+import ReactGA from 'react-ga';
+
 import 'styles/_base.scss';
 
 import store from 'store';
 
 import Api from './api';
 import initStore from './initStore';
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, {testMode: process.env.NODE_ENV === 'test'});
 
 setInterval(async () => {
     let {
