2c328dfadccbe77a3290e992b4eccbfef3e0df73
Dipesh Lamichhane <ft9dipesh@gmail.com>
2023-01-13 16:16:02 +0545

    feat(surveys): draft survey indicator
    Added more prominent draft survey indicator.
    Fix dropdown not closing on double click.
    Add password visibility toggle in login form.

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,5 +1,6 @@
-import {useCallback, useState} from 'react';
+import {useCallback, useState, useMemo} from 'react';
 import {Link, useHistory} from 'react-router-dom';
+import {BsEyeFill, BsEyeSlashFill} from 'react-icons/bs';
 
 import useRequest from 'hooks/useRequest';
 import {dispatchLogin} from 'utils/dispatch';
@@ -9,16 +10,31 @@ import AuthModals from 'components/AuthModals';
 import Button from 'components/Button';
 import Form, {InputField} from '@ra/components/Form';
 import TextInput from '@ra/components/Form/TextInput';
-import SecureTextInput from '@ra/components/Form/SecureTextInput';
+import Input from '@ra/components/Form/Input';
 import {Localize} from '@ra/components/I18n';
 import {_} from 'services/i18n';
 
+import cs from '@ra/cs';
 import initStore from 'services/initStore';
 import useAuthModals from 'hooks/useAuthModals';
 
 import logo from 'assets/images/logo-dark.svg';
 
 import styles from './styles.scss';
+
+export const PasswordInput = props => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = useCallback(() => setShowPassword(sp => !sp), []);
+    const Icon = useMemo(() => showPassword ? BsEyeSlashFill : BsEyeFill, [showPassword]);
+
+    return (
+        <div className={styles.passwordInputContainer}>
+            <Input {...props} className={cs(styles.passwordInput, props.className)} type={showPassword ? 'text' : 'password'} />
+            <Icon className={styles.passwordInputIcon} onClick={toggleShowPassword} />
+        </div>
+    );
+};
 
 const Login = () => {
     const authModalsConfig = useAuthModals();
@@ -86,7 +102,7 @@ const Login = () => {
                                 />
                                 <InputField
                                     label={_('Password')}
-                                    component={SecureTextInput}
+                                    component={PasswordInput}
                                     name='password'
                                     required
                                     className={styles.input}
